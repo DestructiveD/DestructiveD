64bc20ac3af539c1ed2428f407a57fd8eb81067c
Destructive <161433401+DestructiveD@users.noreply.github.com>
2024-02-27 10:29:12 +0200

    D.E.S v1.2
    The D.E.S program.

--- Des.py
+++ Des.py
@@ -3,10 +3,20 @@ import os
 import sys
 import time
 import re
+cmds = ["exit","clear","ifconfig","echo","help"]
 while True:
   cmd input("% ")
   if cmd == "exit":
     sys.exit
+  elif cmd == "clear":
+    os.system("clear")
+  elif cmd == "ifconfig":
+    os.system("ifconfig")
+  elif cmd[:4] == "echo":
+    print(cmd[5:])
+  elif cmd == "help":
+    for i in cmds:
+      print(i)
   else:
     if len(cmd.strip()) != 0:
       print(cmd,"is not a valid command.")
