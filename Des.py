eefc33dccc19365d09e85a1f49d1514969e039cf
Destructive <161433401+DestructiveD@users.noreply.github.com>
2024-02-27 10:38:34 +0200

    D.E.S v1.2.98
    The D.E.S program.

--- Des.py
+++ Des.py
@@ -4,6 +4,9 @@ import sys
 import time
 import re
 cmds = ["exit","clear","ifconfig","echo","help"]
+letterGenerate = random.randint(11111111,99999999)
+letterGenerate = str(letterGenerate)
+letterGenerate = letterGenerate.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5","E").replace("6","F").replace("7","G").replace("8","H").replace("9","I")
 while True:
   cmd input("% ")
   if cmd == "exit":
@@ -17,6 +20,9 @@ while True:
   elif cmd == "help":
     for i in cmds:
       print(i)
+  elif cmd[:8] == "generate":
+    if cmd[9:] == "letter":
+      print(letterGenerate)
   else:
     if len(cmd.strip()) != 0:
       print(cmd,"is not a valid command.")
